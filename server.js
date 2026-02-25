f00b14dc926cb836cdbd4c57407fad8dd49bd1da
Vignesh160899 <vignesh19v@gmail.com>
2026-02-25 17:39:48 +0530

    Fix CORS for production

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors()); // Pre-flight for all routes
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
